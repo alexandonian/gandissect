7a4e721e7ab640e16b1406f5273d19a62fc88dcc
Alex Andonian <alexandonian@gmail.com>
2019-12-05 05:26:16 -0500

    Add script to dissect all places categories

---
+++ script/dissect_all_biggan_scenes.py
@@ -0,0 +1,57 @@
+import os
+
+with open('/data/datasets/Places365/categories_places365.txt') as f:
+        categories = ['-'.join(x.split(' ')[0].split('/')[2:]) for x in f]
+
+cmd_tmpl = """
+python -m netdissect \
+    --gan \
+    --model "netdissect.biggan.pretrained_biggan(res=128, pretrained='places365', y={})" \
+    --outdir "dissect/biggan128-places365-{}-{}" \
+    --layer blocks.0.0 blocks.1.0 blocks.2.0 blocks.3.0 blocks.4.0  \
+    --batch_size 1 \
+    --size 1000 \
+    --z_dim 120
+"""
+
+for i, cat in enumerate(categories):
+    print(f'Doing scene {i}: {cat}')
+    cmd = cmd_tmpl.format(i, i, cat)
+    os.system(cmd)
+
+# # Start from parent directory of script
+# cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+# for SCENE in \
+#     livingroom
+# do
+#     echo "Doing ${SCENE}, all layers"
+#     PTHFILE="models/karras/${SCENE}_lsun.pth"
+#     python -m netdissect \
+#       --gan \
+#       --model "netdissect.proggan.from_pth_file('${PTHFILE}')" \
+#       --outdir "dissect/${SCENE}" \
+#       --layer layer1 layer2 layer3 layer4 layer5 layer6 layer7 \
+#               layer8 layer9 layer10 layer11 layer12 layer13 layer14 \
+#       --size 1000
+# done
+
+# # Run dissection on each scene
+# for SCENE in \
+#     churchoutdoor \
+#     bedroom \
+#     conferenceroom \
+#     kitchen \
+#     diningroom \
+#     restaurant
+# do
+#     echo "Doing ${SCENE}"
+#     PTHFILE="models/karras/${SCENE}_lsun.pth"
+#     python -m netdissect \
+#       --gan \
+#       --model "netdissect.proggan.from_pth_file('${PTHFILE}')" \
+#       --outdir "dissect/${SCENE}" \
+#       --layer layer4 \
+#       --size 1000
+# done
+
